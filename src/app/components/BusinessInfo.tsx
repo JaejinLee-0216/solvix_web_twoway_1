3b4e9fb9d191c0d00c509e542243be8a47a55ed2
JaejinLee-0216 <victoryljj216@naver.com>
2025-11-07 15:56:42 +0900

    Update BusinessInfo component

--- src/app/components/BusinessInfo.tsx
+++ src/app/components/BusinessInfo.tsx
@@ -11,8 +11,7 @@ export default function BusinessInfo({ className = "", align = "center" }: Busin
       className={`text-[10px] leading-[1.5] tracking-[-0.01em] text-[#8F96A6] ${alignmentClass} ${className}`.trim()}
     >
       <p>
-        (주)NeoQ (대표: 이재진, 김시은) | 사업자등록번호: 162-30-02074 | 사업장 주소: 서울 송파구 위례서이로 25 | E-mail:
-        neoqstudy@gmail.com
+        (주)NeoQ (대표: 김시은) | 사업자등록번호: 162-30-02074 | 사업장 주소: 서울 송파구 위례서이로 25 | E-mail: neoqstudy@gmail.com | 전화번호: 010-5450-6393
       </p>
       <p className="mt-1">Copyright © 2025 NeoQ Inc. All rights reserved.</p>
     </div>
